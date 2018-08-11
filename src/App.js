f200ea715daebb5f8481682f2d966ef4ac9d6487
Fatih Yıldırım <isafatihyildirim@gmail.com>
2018-08-11 16:49:47 +0300

    edited app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/* global chrome, console */
+/* global chrome */
 
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,7 +11,7 @@ import {
   arrayMove,
 } from 'react-sortable-hoc';
 
-import demo from './demo.json';
+// import demo from './demo.json';
 import ListItem from './components/listItem';
 
 const SortableItem = SortableElement(({ value }) => (
@@ -33,21 +33,41 @@ class App extends Component {
     super(props);
 
     this.state = {
-      items: demo[0].children,
+      items: [],
     };
   }
 
   componentWillMount() {
-    // chrome.bookmarks.getTree((itemTree) => {
-    //   this.setState({json: itemTree[0].children})
-    // });
+    chrome.bookmarks.getTree((itemTree) => {
+      this.setState({ items: itemTree[0].children });
+    });
+
+    // this.setState({ items: demo[0].children });
   }
 
-  onSortEnd = () => ({ oldIndex, newIndex }) => {
+  onSortEnd = ({ oldIndex, newIndex }) => {
     this.setState(prevState => ({
       items: arrayMove(prevState.items, oldIndex, newIndex),
-    }));
+    }), this.changeIndexes(oldIndex, newIndex));
   };
+
+  changeIndexes = (oldIndex, newIndex) => () => {
+    let initial = null;
+    let end = null;
+
+    if (oldIndex < newIndex) {
+      initial = oldIndex;
+      end = newIndex;
+    } else {
+      initial = newIndex;
+      end = oldIndex;
+    }
+
+    for (initial; initial <= end; initial += 1) {
+      this.state.items[initial].index = initial;
+    }
+  }
+
 
   render() {
     return (
