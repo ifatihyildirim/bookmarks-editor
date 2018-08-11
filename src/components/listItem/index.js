df311307185b02dfe4ccdde4f56d2d9cb1a8855f
Fatih Yıldırım <isafatihyildirim@gmail.com>
2018-08-11 13:14:29 +0300

    edited list item

--- src/components/listItem/index.js
+++ src/components/listItem/index.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle,Button} from 'reactstrap';
+import {
+  Card, CardImg, CardBody, CardTitle, CardSubtitle,
+} from 'reactstrap';
 
 
-class App extends Component {
-  render() {
-      const { title, url } = this.props;
-      return (
-    <Card >
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>{title}</CardTitle>
-          <CardSubtitle>{url}</CardSubtitle>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-    );
-  }
-}
+const App = (props) => {
+  const { title, url, img } = props;
+
+  return (
+    <Card>
+      <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+      <CardBody>
+        <CardTitle>
+          {title}
+        </CardTitle>
+        <CardSubtitle>
+          <small className="text-muted">
+            <a target="_blank" rel="noopener noreferrer" href={url}>
+              {url}
+            </a>
+          </small>
+        </CardSubtitle>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default App;
